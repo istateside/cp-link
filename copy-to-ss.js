be1da23c467fa64f7863f1dbda2f1f29d793bd9e
Kevin Fleischman <kfleischman@squarespace.com>
2021-03-08 10:06:55 -0500

    first commit

---
+++ copy-to-ss.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+
+function copyToSiteServer() {
+  let currentDir = process.cwd();
+
+  console.log('curr dir is ', currentDir);
+}
+
+copyToSiteServer();
